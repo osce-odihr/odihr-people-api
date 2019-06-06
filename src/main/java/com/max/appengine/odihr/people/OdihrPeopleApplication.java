e7cbd427b21cc253677daa56f1a515c173e0fa43
maxsv0 <max.svistunov@gmail.com>
2019-06-06 19:36:28 +0200

    Naming for app

---
+++ src/main/java/com/max/appengine/odihr/people/OdihrPeopleApplication.java
@@ -0,0 +1,13 @@
+package com.max.appengine.odihr.people;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdihrPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdihrPeopleApplication.class, args);
+	}
+
+}
--- src/test/java/com/max/appengine/odihr/people/OdihrPeopleSApplicationTests.java
+++ src/test/java/com/max/appengine/odihr/people/OdihrPeopleSApplicationTests.java
@@ -1,4 +1,4 @@
-package com.max.appengine.odihr.people.odihrpeoplespringbootappengine;
+package com.max.appengine.odihr.people;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class OdihrPeopleSpringbootAppengineApplicationTests {
+public class OdihrPeopleSApplicationTests {
 
 	@Test
 	public void contextLoads() {
