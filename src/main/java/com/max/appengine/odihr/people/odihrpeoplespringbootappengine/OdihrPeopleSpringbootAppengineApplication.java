87db4dc65c440cd1afa95ce2c0f40c14a1c0936c
maxsv0 <max.svistunov@gmail.com>
2019-06-06 17:40:25 +0200

    Init Spring Boot application

---
+++ src/main/java/com/max/appengine/odihr/people/odihrpeoplespringbootappengine/OdihrPeopleSpringbootAppengineApplication.java
@@ -0,0 +1,13 @@
+package com.max.appengine.odihr.people.odihrpeoplespringbootappengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdihrPeopleSpringbootAppengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdihrPeopleSpringbootAppengineApplication.class, args);
+	}
+
+}
